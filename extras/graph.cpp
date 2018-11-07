0572ef2457ec8cc7170c6d11334ac05dfa198a74
Fuad Al Abir <amusafeer@gmail.com>
2018-11-07 23:42:26 +0600

    Adjacency Matrix and Adjacency List - Done

--- extras/graph.cpp
+++ extras/graph.cpp
@@ -4,7 +4,7 @@
 Author: Fuad Al Abir
 Dated: November 7, 2018
 Name: graph.cpp
-Objective: This program creates Adjacency Matrix from a graph (Directed and un-directed). Pairwise connected nodes are the inputs.
+Objective: This program creates Adjacency Matrix and Adjacency list from a - Directed and un-directed - graph. Pairwise connected nodes are the inputs.
 Algorithms: Brute force
 Problem Source: https://www.geeksforgeeks.org/graph-and-its-representations/
 Instructor: None
@@ -15,21 +15,33 @@ Instructor: None
 Header: iostream
 Reason: Input/Output stream - cin, cout
 */
-
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+/*-------------------------------------------------
+    U S E R   D E F I N E D   F U N C T I O N S
+---------------------------------------------------
+Function:   void addEdge(vector <int> adjList[], int u, int v);
+Reason:     adding the edges to adjacency list
+*/
+void addEdge(vector <int> adjList[], int u, int v);
+
 int main()
 {
-    int node;                       // # of nodes of the graph
-    int edge;                       // # of egdes of the graph
+    int node;                      // # of nodes of the graph
+    int edge;                      // # of egdes of the graph
+    int row;                       // variable row used in setting up adjoint matrix
+    int col;                       // variable col used in setting up adjoint matrix
     cout << "# of node(s): ";
     cin >> node;
     cout << "# of edge(s): ";
     cin >> edge;
+
     int graph[edge][2];             // graph Matrix with #egde rows and two columns
     int adjMat[node][node];         // adjMat Matrix with #node rows and #node columns
+    vector <int> adjList[node];
 
     // Initializing all the rows and cols of graph Matrix to 0
     for(int i = 0; i < edge; i++)
@@ -58,8 +70,14 @@ int main()
         }
     }
 
-    int row = -1;
-    int col = -1;
+    // Adding egdes in adjacency list
+    for(int i = 0; i < edge; i++)
+    {
+        addEdge(adjList, graph[i][0], graph[i][1]);
+    }
+
+    row = -1;
+    col = -1;
 
     cout << "\nNodes with Edges: ";
     
@@ -68,7 +86,11 @@ int main()
         for(int j = 0; j < 2; j++)
         {
             // printing the connected nodes pairwise
-            if(j == 0)
+            if(i == 0 && j == 0)
+            {
+                cout << "{{" << graph[i][j] << ", ";
+            }
+            else if(j == 0)
             {
                 cout << "{" << graph[i][j] << ", ";
             }
@@ -78,7 +100,7 @@ int main()
             }
             else
             {
-                cout << graph[i][j] << "}" << endl;
+                cout << graph[i][j] << "}}" << endl;
             }
             // detecting the rows and cols that are connected
             if(j == 0) row = graph[i][j];
@@ -100,6 +122,22 @@ int main()
         }
         cout << endl;
     }
-    
+    // printing Adjacency List
+    for (int i = 0; i < node; i++) 
+    { 
+        cout << "\nAdjacency list of vertex: " << i << "\nhead";
+        for (int j = 0; j < adjList[i].size(); j++)
+        {
+           cout << " -> " << adjList[i][j];
+        } 
+        cout << endl;
+    }
     return 0;
+}
+
+void addEdge(vector <int> adjList[], int u, int v)
+{
+    adjList[u].push_back(v);
+    /*** For Directed graph, one have just to eleminate or comment-out the following single line ***/
+    adjList[v].push_back(u);
 }
