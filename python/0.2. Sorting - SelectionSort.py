90c0fd731ba78af845e3d9d913a4db1f2154d58d
Fuad Al Abir <31713168+FuadAlAbir@users.noreply.github.com>
2019-03-27 22:55:23 +0600

    Update 0.2. Sorting - SelectionSort.py

--- python/0.2. Sorting - SelectionSort.py
+++ python/0.2. Sorting - SelectionSort.py
@@ -1,11 +1,12 @@
 def SelectionSort(A):
-    for i in range (len(A)):
+    for i in range (len(A) - 1):
+        k = i
         for j in range (i+1, len(A)):
-            if(A[j] < A[i]):
-                A[j], A[i] = A[i], A[j]
+            if(A[j] < A[k]):
+                k = j
+        A[i], A[k] = A[k], A[i]
+        print(A)
                 
-A = [21, 34, 52, 11, 8, 51, 3]
-#A = [3, 8, 11, 21, 34, 51, 52]
+A = [2, 7, 4, 1, 5, 3]
 print(A)
 SelectionSort(A)
-print(A)
