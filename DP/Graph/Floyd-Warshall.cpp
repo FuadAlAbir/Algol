c47d09709fbed76aedc439c316113dc83693b874
Fuad Al Abir <31713168+FuadAlAbir@users.noreply.github.com>
2019-03-29 03:46:10 +0600

    Create Floyd-Warshall.cpp

---
+++ DP/Graph/Floyd-Warshall.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+#define INF 999
+
+using namespace std;
+
+/*
+    A[node][node] = {   {0, 3, INF, 7},
+                        {8, 0, 2, INF},
+                        {5, INF, 0, 1},
+                        {2, INF, INF, 0}    };
+                        
+S A M P L E   I N P U T
+4
+0 3 999 7
+8 0 2 999
+5 999 0 1
+2 999 999 0
+
+S A M P L E   O U T P U T
+0 3 5 6
+5 0 2 3
+3 6 0 1
+2 5 7 0
+*/
+
+int main()
+{
+    int node;
+
+    cin >> node;
+    int A[node][node];
+
+    // USER-INPUT
+    for(int i = 0; i < node; i++)
+    {
+        for(int j = 0; j < node; j++)
+        {
+            cin >> A[i][j];
+        }
+    }
+    
+    // FLOYD-WARSHALL Algo
+    for(int k = 1; k <= node; k++)
+    {
+        for(int i = 0; i < node; i++)
+        {
+            for(int j = 0; j < node; j++)
+            {
+                A[i][j] = min( A[i][j], A[i][k - 1] + A[k - 1][j] );
+            }
+        }
+    }
+    
+    // PRINT RESULT
+    for(int i = 0; i < node; i++)
+    {
+        for(int j = 0; j < node; j++)
+        {
+            cout << A[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
